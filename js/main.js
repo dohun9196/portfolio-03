49caf8d26e96df7a9c1609ab23d794208fbe4204
Dohun9196 <68097610+dohun9196@users.noreply.github.com>
2022-12-12 17:43:27 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+
+    $('.main_slide').slick({
+        dots: true,
+        autoplay: false,
+        arrows: true,
+
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        pauseOnFocus: false,
+        prevArrow: false,
+        nextArrow: false,
+
+
+        infinite: true,
+        speed: 1500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+});
