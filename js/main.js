9122c14f14a94ea16cc0140ac82f5853c4b8ea53
Dohun9196 <68097610+dohun9196@users.noreply.github.com>
2022-12-15 20:55:37 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,23 +16,8 @@ $(function () {
         speed: 1500,
         fade: true,
         cssEase: 'linear'
+
+
     });
 
-
-    $('.top_btn a').click(function () {
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 500);
-        return false;
-    });
-
-    $(window).scroll(function () {
-
-        if ($(this).scrollTop() < 900) {
-            $("#top_btn").css('visibility', 'hidden');
-        }
-        else {
-            $("#top_btn").css('visibility', 'visible');
-        }
-    });
 });
